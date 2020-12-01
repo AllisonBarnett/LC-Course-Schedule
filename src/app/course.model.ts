f3e9328e42ff85ab6c7095f31e68f3454865f8cb
Allison <abarnett17@gmail.com>
2020-11-30 20:16:33 -0600

    Custom CSS added; getDays method added to Course class.

--- src/app/course.model.ts
+++ src/app/course.model.ts
@@ -1,4 +1,8 @@
 export class Course {
     constructor(public title: string, public code: string, public days: string[], 
         public Starttime:string, public endTime: string, public credits: number){}
+
+    getDays(){
+        return this.days.join("/");
+    }
 }
