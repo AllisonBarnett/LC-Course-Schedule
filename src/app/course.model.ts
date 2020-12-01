efa8cfdd4df3e16166ccc8d5215917b136f7c70f
Allison <abarnett17@gmail.com>
2020-11-30 19:40:14 -0600

    Course classs defined

--- src/app/course.model.ts
+++ src/app/course.model.ts
@@ -1,2 +1,4 @@
 export class Course {
+    constructor(public title: string, public code: string, public days:[], 
+        public Starttime:string, public endTime: string, public credits: number){}
 }
