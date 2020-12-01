23f81cd750fc5dc843d95d7a9510935a86957947
Allison <abarnett17@gmail.com>
2020-11-30 19:27:44 -0600

    Page layout set up in app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { CoursesComponent } from './courses/courses.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ScheduleComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule
